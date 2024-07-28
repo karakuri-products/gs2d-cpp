31f5b0579525a811c7e048deddc43854ed94438e
realteck-ky <32591276+realteck-ky@users.noreply.github.com>
2024-07-29 00:14:33 +0900

    Fix ArduinoSerial.h format

--- examples/arduino/ArduinoSerial.h
+++ examples/arduino/ArduinoSerial.h
@@ -1,57 +1,60 @@
 /*
-* @file    WindowsSerial.h
-* @author
-* @date    2020/04/05
-* @brief   Arduino Serial Base Class
-*/
+ * @file    WindowsSerial.h
+ * @author
+ * @date    2020/04/05
+ * @brief   Arduino Serial Base Class
+ */
 #pragma once
 
 /* Includes ------------------------------------------------------------------*/
 #include <Arduino.h>
 
 /* Variables -----------------------------------------------------------------*/
-	class ArduinoSerial
-	{
-	public:
-		ArduinoSerial() {  }
-		~ArduinoSerial() {}
 
-		int open(void)
-		{
-      pinMode(2, OUTPUT);
-      Serial1.begin(115200, SERIAL_8E1);
-//      Serial1.begin(115200, SERIAL_8N1);
-		}
+class ArduinoSerial
+{
+public:
+    ArduinoSerial() {}
+    ~ArduinoSerial() {}
 
-		int isConnected(void)
-		{
-			return 1;
-		}
+    int open(void)
+    {
+        pinMode(2, OUTPUT);
+        Serial1.begin(115200, SERIAL_8E1);
+        //      Serial1.begin(115200, SERIAL_8N1);
+    }
 
-		void close(void)
-		{
-		}
-   
-		int read(void)
-		{
-      if(Serial1.available())
-      {
-        return Serial1.read();        
-      }else{
-        return -1;
-      }
-		}
+    int isConnected(void)
+    {
+        return 1;
+    }
 
-		int write(unsigned char* data, uint8_t size)
-		{
-      digitalWrite(2, HIGH);
-		  int retval = Serial1.write(data, size);
-      Serial1.flush();
-      digitalWrite(2, LOW);
-		}
+    void close(void)
+    {
+    }
 
-		unsigned long long int time(void)
-		{
-			return millis();
-		}
-	};
+    int read(void)
+    {
+        if (Serial1.available())
+        {
+            return Serial1.read();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    int write(unsigned char *data, uint8_t size)
+    {
+        digitalWrite(2, HIGH);
+        int retval = Serial1.write(data, size);
+        Serial1.flush();
+        digitalWrite(2, LOW);
+    }
+
+    unsigned long long int time(void)
+    {
+        return millis();
+    }
+};
