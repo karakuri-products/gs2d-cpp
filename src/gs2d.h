32f0514db7dcf78c2ce37f6b70959d81ad600921
realteck-ky <32591276+realteck-ky@users.noreply.github.com>
2024-07-30 08:41:44 +0900

    Add gs2d.h to include all headers

---
+++ src/gs2d.h
@@ -0,0 +1,15 @@
+/*
+* @file    gs2d.h
+* @author
+* @date    2024/07/28
+* @brief   Top of the headers for users
+*/
+#pragma once
+
+/* Includes ------------------------------------------------------------------*/
+#include "gs2d_type.h"
+#include "gs2d_robotis.h"
+#include "gs2d_futaba.h"
+#include "gs2d_b3m.h"
+#include "gs2d_krs.h"
+#include "gs2d_util_serial_base.h"
